42d791289664b5b6efcba4ec1a73efa36ffefe76
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2016-12-28 16:04:53 +0000

    Better function naming

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -2,15 +2,15 @@ defmodule Issues.TableFormatter do
   @date_length 20
 
   def format(data) do
-    id_length = longest_element_length(data, "id")
-    title_length = longest_element_length(data, "title")
+    id_length = longest_by_field_length(data, "id")
+    title_length = longest_by_field_length(data, "title")
     header = create_header(id_length, title_length)
     top_line = create_top_line(id_length, title_length)
     body = create_body(data, id_length, title_length)
     "#{header}#{top_line}#{body}"
   end
 
-  defp longest_element_length(data, field) do
+  defp longest_by_field_length(data, field) do
     Enum.max_by(data, &(String.length(to_string(&1[field]))))[field]
     |> to_string
     |> String.length
@@ -26,7 +26,7 @@ defmodule Issues.TableFormatter do
 
   defp create_body(data, id_length, title_length) do
     Enum.map(data, fn(issue) ->
-      "#{String.pad_trailing(Integer.to_string(issue["id"]), id_length)} | #{String.pad_trailing(issue["created_at"], @date_length)} | #{String.pad_trailing(issue["title"], title_length)}\n"
+      "#{String.pad_trailing(to_string(issue["id"]), id_length)} | #{String.pad_trailing(issue["created_at"], @date_length)} | #{String.pad_trailing(issue["title"], title_length)}\n"
     end)
   end
 end
