cad6d5776e95fd4f41dcada1cb4221fd95654e6c
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2016-12-30 15:12:59 +0000

    Add logging

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,14 +1,19 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir ljones140@gmail.com"} ]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
   end
 
   def issues_url(user, project) do
+    Logger.info "Successful repsonse"
+    Logger.debug fn -> inspect(body) end
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
@@ -16,7 +21,8 @@ defmodule Issues.GithubIssues do
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response({ _, %{status_code: _, body: body}}) do
+  def handle_response({ _, %{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
     { :error, Poison.Parser.parse!(body) }
   end
 end
