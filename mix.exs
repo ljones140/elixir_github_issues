00d96d266f5d3ded55172fef902e640e8ee2454f
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2016-12-30 16:04:54 +0000

    Created ExDoc documentation

--- mix.exs
+++ mix.exs
@@ -3,6 +3,8 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     name: "Issues",
+     source_url: "https://github.com/ljones140/elixir_github_issues",
      escript: escript_config,
      version: "0.1.0",
      elixir: "~> 1.3",
@@ -29,8 +31,10 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.8",
-      poison:    "~> 1.5"
+      httpoison:  "~> 0.8",
+      poison:     "~> 1.5",
+      ex_doc:     "~> 0.11",
+      earmark:    ">= 0.0.0"
     ]
   end
 
